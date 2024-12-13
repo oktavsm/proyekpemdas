8f6261e084bfc7db3b306de6c00a87d2a09e2c95
oktavsm <incdmincs@gmail.com>
2024-12-13 15:32:51 +0700

    Add files via upload

---
+++ projectpemdas/test.java
@@ -0,0 +1,17 @@
+public class test {
+
+  public static void main(String[] args) {
+    java.util.Scanner in = new java.util.Scanner(System.in);
+    int n = in.nextInt();
+    iterasi(n);
+  }
+  public static void interasi(int n) {
+    for(int i = 0; i < n ;i++){
+      
+    }
+    
+  }
+  public static void subiterasi(){
+
+  }
+}
